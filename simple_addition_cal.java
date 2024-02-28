7dc55e4679cff38640c6ae21b55592065a6d855e
MadhukeshwarPatil <madhukeshwarpatil@gmail.com>
2024-02-28 21:44:52 +0530

    added simple java calculator file to add 2 numbers

---
+++ simple_addition_cal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  
+class SumOfNumbers2  
+{  
+    public static void main(String args[])  
+    {  
+        int x, y, sum;  
+        Scanner sc = new Scanner(System.in);  
+        x = sc.nextInt();  
+        y = sc.nextInt(); 
+        sum = sum(x, y);  
+        System.out.println("The sum of numbers " + x + " and " + y + " is: " +sum );  
+    }   
+    public static int sum(int a, int b)  
+    {  
+        int sum = a + b;  
+        return sum;  
+    }  
+}  
